9031350bf0b8f76c3441b4f294c63ebae9361a10
kramonex <kramonex1@yandex.ru>
2023-05-05 21:25:20 +0300

    save

--- Networking/Anime.swift
+++ Networking/Anime.swift
@@ -7,16 +7,36 @@
 
 import Foundation
 
+//struct AnimeList: Decodable {
+//    let animes: [Anime]
+//}
 
 struct Anime: Decodable {
-    let russian: String
-    let image: Image
-    let kind: String
-    let score: String
-    let status: String
+    let id: Int
+//    let russian: String
+//    let image: String
+//    let kind: String
+//    let score: String
+//    let status: String
 }
 
 struct Image: Decodable {
     let original: String
+}
+
+
+
+
+struct Course: Decodable {
+    let name: String
+    let imageUrl: URL
+    let number_of_lessons: Int
+    let number_of_tests: Int
+}
+
+struct SwiftBookInfo: Decodable {
+    let courses: [Course]
+    let websiteDescription: String
+    let websiteName: String
 }
 
--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -9,38 +9,65 @@ import UIKit
 
 enum Link {
     case mostPopular
+        case test
     
     var url: URL {
         switch self {
         case .mostPopular:
             return URL(string:
-                        "https://shikimori.me/api/animes?season=spring_2023&kind=tv&order=popularity&limit=1)")!
+                        "https://shikimori.me/api/animes?season=spring_2023&kind=tv&order=popularity&limit=2)")!
+        case .test:
+            return URL(string: "https://swiftbook.ru//wp-content/uploads/api/api_courses")!
         }
     }
 }
-
+    
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         fetchAnime()
+        //        fetchTest()
     }
     
+}
+
+extension ViewController {
     private func fetchAnime() {
-        URLSession.shared.dataTask(with: Link.mostPopular.url) { data, _, error in
-            guard let data = data else {
+        URLSession.shared.dataTask(with: Link.mostPopular.url) { data, _,
+            error in
+            guard let data else {
                 print(error?.localizedDescription ?? "No error description")
                 return
             }
             
             do {
                 let decoder = JSONDecoder()
-                let animeList = try decoder.decode(Anime.self, from: data)
-                print(animeList)
+                let animes = try decoder.decode([Anime].self, from: data)
+                print(animes)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    private func fetchTest() {
+        URLSession.shared.dataTask(with: Link.test.url) { data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let test = try decoder.decode([Course].self, from: data)
+                print(test)
             } catch {
                 print(error)
             }
         }.resume()
     }
 }
+
+
